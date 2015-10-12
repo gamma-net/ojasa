36c99cf00bd96efe9bd441a8a97b3ddb6bcfa63e
Jaffer Asegaf <jafferasegaf@JafferAcBookPro.(none)>
2015-10-12 10:38:02 +0700

    revert redirection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -71,11 +71,12 @@ class ApplicationController < ActionController::Base
         end unless role.permissions.blank?
       end
       
-      redirect_to admin_home_url and return
+      redirect_to admin_home_url and return false
     end
     # helper_method :validate_admin_permission
 
     def admin_home_url
       return admin_contents_news_index_url unless request.path == '/admin/contents/news'
+      return '/'
     end
 end
