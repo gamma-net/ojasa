df44dbe6a2ba76183305afdb0792f6ee78b59302
Jaffer Asegaf <jafferasegaf@Jaffer-Asegafs-MacBookPro.local>
2015-11-04 13:13:40 +0700

    add location constraint

--- app/helpers/services_helper.rb
+++ app/helpers/services_helper.rb
@@ -3,8 +3,8 @@ module ServicesHelper
     Service::FEE_TYPES.collect {|k,v| [v, k]}
   end
   
-  def service_options(category_id)
+  def service_options(category_id, location=nil)
     return [] unless category_id
-    [['-------------------', '']] + Service.where(category_id: category_id).active.collect {|s| [s.name, s.id]}
+    [['-------------------', '']] + Service.where(category_id: category_id).where(location ? "location LIKE '%#{location}%'" : "1=1").active.collect {|s| ["#{s.name} (#{s.contact_number})", s.id]}
   end
 end
